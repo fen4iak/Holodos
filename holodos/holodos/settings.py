82d541c068b6a641e439f185604a795b093e3d9a
gotar <gotartartar@gmail.com>
2026-04-19 16:43:38 +0300

    тест

--- holodos/holodos/settings.py
+++ holodos/holodos/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'holodos.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'holodos/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
